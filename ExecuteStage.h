f7299965d3820433001d005085be65de1598ca99
Taylor King <kingtb@appstate.edu>
2015-11-13 16:21:12 -0500

    passed all the tests

--- ExecuteStage.h
+++ ExecuteStage.h
@@ -7,8 +7,9 @@ private:
     uint64_t alufun(uint64_t e_icode, uint64_t e_ifun);
     bool set_cc(uint64_t e_icode);
     uint64_t e_dstE(uint64_t e_icode, uint64_t e_dstE, uint64_t e_cnd);
-    void setMInput(M * mreg, uint64_t stat, uint64_t icode, uint64_t valE, uint64_t valA, uint64_t dstE, uint64_t dstM);
+    void setMInput(M * mreg, uint64_t stat, uint64_t icode, uint64_t cond, uint64_t valE, uint64_t valA, uint64_t dstE, uint64_t dstM);
     uint64_t exec_alu(uint64_t alu_fun, uint64_t aluA, uint64_t aluB, bool setcc);
+    uint64_t cond(uint64_t icode, uint64_t ifun);
 public:
     bool doClockLow(PipeRegister ** pregs, Stage ** stages) ;
     void doClockHigh(PipeRegister ** pregs);
--- WritebackStage.h
+++ WritebackStage.h
@@ -1,7 +1,10 @@
 
 class WritebackStage: public Stage {
+private:
+    bool mem_read(uint64_t icode); 
 public:
     bool doClockLow(PipeRegister ** pregs, Stage ** stages) ;
     void doClockHigh(PipeRegister ** pregs);
+    void valM_toReg(uint64_t valM, uint64_t dstM);
 };
 
